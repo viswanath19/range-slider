5704e837fcb9d4029395d787c4b01b966f4ce0be
ViswanathSarma <viswanath.sarma@acquire.io>
2021-08-19 16:04:29 +0530

    implement controls

--- src/components/RangeSlider/slider.stories.js
+++ src/components/RangeSlider/slider.stories.js
@@ -1,8 +1,26 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
-import ExampleRangeSlider from '../RangeSlider/example';
 
-storiesOf('RangeSlider', module)
-  .add('basical', () => (
-    <ExampleRangeSlider />
-));
+import RangeSlider from '../RangeSlider';
+import { useArgs } from '@storybook/client-api';
+
+export default {
+  component: RangeSlider,
+  title: 'Slider',
+};
+
+const Template = args => {
+  const [{progressValue}, updateArgs] = useArgs("100");
+  const toggleValue = (val) => {
+    updateArgs({progressValue:parseInt(val)})
+  }
+  return <RangeSlider {...args} onChange={(event,value)=>toggleValue(value)} />;
+}
+
+export const Default = Template.bind({});
+Default.args = {
+  progressValue: 100,
+};
+Default.argTypes = {
+  progressValue: { control: { type: 'number' } },
+  onChange: { action: 'Changed!' },
+};
